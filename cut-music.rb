304c812136b54847b34d2256e036107e0fb3cdb9
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-03-20 19:37:19 +0900

    fix: :hammer: Fixed cut-music.rb

--- cut-music.rb
+++ cut-music.rb
@@ -1,15 +1,14 @@
 # Usage: ruby cut-music.rb MP3_DIR START_TIME END_TIME
 # Requirements: ruby, ffmpeg
-# Example: ruby cut-music.rb MP3_DIR START_TIME END_TIME
+# Example: ruby cut-music.rb MP3_DIR CUT_DIR START_TIME END_TIME
 MP3_DIR = ARGV[0]
-START_TIME = ARGV[1]
-END_TIME = ARGV[2]
-
-MUSIC_DIR = "music"
+CUT_DIR = ARGV[1]
+START_TIME = ARGV[2]
+END_TIME = ARGV[3]
 
 # Check if the user has provided the required arguments
-if MP3_DIR.nil? || START_TIME.nil? || END_TIME.nil?
-  puts "Usage: ruby cut-music.rb MP3_DIR START_TIME END_TIME"
+if MP3_DIR.nil? || CUT_DIR.nil? || START_TIME.nil? || END_TIME.nil?
+  puts "Usage: ruby cut-music.rb MP3_DIR CUT_DIR START_TIME END_TIME"
   exit
 end
 
@@ -17,6 +16,11 @@ end
 if Dir.exist?(MP3_DIR) == false
   puts "The directory #{MP3_DIR} does not exist"
   exit
+end
+
+# Check if the CUT_DIR exists
+if Dir.exist?(CUT_DIR) == false
+  Dir.mkdir(CUT_DIR)
 end
 
 # Check START_TIME and END_TIME are integers
@@ -36,6 +40,6 @@ end
 
 # Cut the music files
 mp3_files.each do |mp3_file|
-  music_file = "#{MUSIC_DIR}/#{File.basename(mp3_file, ".mp3")}.mp3"
-  system("ffmpeg -i '#{mp3_file}' -ss #{START_TIME} -t #{END_TIME} -acodec copy '#{music_file}'")
+  cut_file = "#{CUT_DIR}/#{File.basename(mp3_file, ".mp3")}.mp3"
+  system("ffmpeg -i '#{mp3_file}' -ss #{START_TIME} -t #{END_TIME} -acodec copy '#{cut_file}'")
 end
