056fa7b63aa8e1730aac03baf78ec1be07aa55b8
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-05 21:17:38 +0900

    fix: :hammer: Fixed createdAt

--- script.js
+++ script.js
@@ -120,9 +120,7 @@ async function addAudioEvent(title, path) {
 function formatDate(createdAt) {
 	const date = new Date(createdAt);
 	const year = date.getFullYear();
-	const month = date.getMonth() + 1;
-	const day = date.getDate();
-	return `${year}年${month}月${day}日`;
+	return `${year}年`;
 }
 
 createMusicList();
